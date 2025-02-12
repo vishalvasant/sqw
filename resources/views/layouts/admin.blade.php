c6bb38c975aaa21c702a6b5a48dd6a2ebc4ab9dc
vishalvasant <vishalvasant8@gmail.com>
2025-02-12 18:41:38 +0530

    Design Changes For Logut

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,13 +14,39 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
         <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-            </ul>
-        </nav>
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+        </ul>
+
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+            <!-- User Dropdown Menu -->
+            <li class="nav-item dropdown">
+                <a class="nav-link" data-toggle="dropdown" href="#">
+                    <i class="fas fa-user"></i>
+                    <span class="ml-2">{{ Auth::user()->name ?? 'User' }}</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <a href="#" class="dropdown-item">
+                        <i class="fas fa-user-circle mr-2"></i> Profile
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-item"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt mr-2"></i> Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.navbar -->
+
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
@@ -31,11 +57,7 @@
         <div class="content-wrapper">
             <div class="content-header">
                 <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">@yield('page-title', 'Dashboard')</h1>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
 
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,10 @@
 @php
     $usr = Auth::guard('web')->user();
- @endphp
+@endphp
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
     <a href="/" class="brand-link">
-        <!-- <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
         <span class="brand-text font-weight-light">SHREEJI QUARRY WORKS</span>
     </a>
-
-    <!-- Sidebar Menu -->
     <div class="sidebar">
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -20,14 +16,12 @@
                     </a>
                 </li>
                 @endif
-                <!-- Inventory Module -->
+                
+                @if ($usr->can('products.view') || $usr->can('categories.view') || $usr->can('units.view'))
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-cogs"></i>
-                        <p>
-                            Inventory
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
+                        <p>Inventory<i class="right fas fa-angle-left"></i></p>
                     </a>
                     <ul class="nav nav-treeview">
                         @if ($usr->can('products.view'))
@@ -46,89 +40,95 @@
                             </a>
                         </li>
                         @endif
+                        @if ($usr->can('units.view'))
                         <li class="nav-item">
                             <a href="{{ route('inventory.units.index') }}" class="nav-link">
                                 <i class="fas fa-ruler nav-icon"></i>
                                 <p>Units</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="{{ route('inventory.warehouses.index') }}" class="nav-link">
-                                <i class="fas fa-building nav-icon"></i>
-                                <p>Warehouses</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('inventory.stock-imports.index') }}" class="nav-link">
-                                <i class="fas fa-arrow-circle-up nav-icon"></i>
-                                <p>Stock Imports</p>
-                            </a>
-                        </li>
+                        @endif
                     </ul>
                 </li>
+                @endif
                 
-                <!-- Purchase Management Module -->
+                @if ($usr->can('suppliers.view') || $usr->can('requests.view') || $usr->can('orders.view'))
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-shopping-cart"></i>
                         <p>Purchase<i class="right fas fa-angle-left"></i></p>
                     </a>
                     <ul class="nav nav-treeview">
+                        @if ($usr->can('suppliers.view'))
                         <li class="nav-item">
                             <a href="{{ route('purchase.suppliers.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Suppliers</p>
                             </a>
                         </li>
+                        @endif
+                        @if ($usr->can('requests.view'))
                         <li class="nav-item">
                             <a href="{{ route('purchase.requests.index') }}" class="nav-link">
                                 <i class="fas fa-list nav-icon"></i>
                                 <p>Purchase Requests-Orders</p>
                             </a>
                         </li>
+                        @endif
+                        @if ($usr->can('orders.view'))
                         <li class="nav-item">
                             <a href="{{ route('purchase.orders.index') }}" class="nav-link">
                                 <i class="fas fa-file-invoice nav-icon"></i>
                                 <p>Stock In</p>
                             </a>
                         </li>
+                        @endif
                     </ul>
                 </li>
+                @endif
                 
-                <!-- Fleet Management Module -->
+                @if ($usr->can('fuel_usages.view') || $usr->can('vehicles.view') || $usr->can('drivers.view') || $usr->can('reports.view'))
                 <li class="nav-item has-treeview">
-                    <a href="{{ route('fleet.vehicles.index') }}" class="nav-link">
+                    <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-truck"></i>
                         <p>Fleet Management<i class="right fas fa-angle-left"></i></p>
                     </a>
                     <ul class="nav nav-treeview">
+                        @if ($usr->can('vehicles.view'))
                         <li class="nav-item">
                             <a href="{{ route('fleet.vehicles.index') }}" class="nav-link">
                                 <i class="nav-icon fas fa-car"></i>
                                 <p>Vehicles</p>
                             </a>
                         </li>
+                        @endif
+                        @if ($usr->can('drivers.view'))
                         <li class="nav-item">
                             <a href="{{ route('fleet.drivers') }}" class="nav-link">
                                 <i class="nav-icon fas fa-user"></i>
                                 <p>Drivers</p>
                             </a>
                         </li>
+                        @endif
+                        @if ($usr->can('fuel_usages.view'))
                         <li class="nav-item">
-                            <a href="{{ route('fleet.fuel_usages.index') }}" class="nav-link {{ request()->routeIs('fuel_usages.*') ? 'active' : '' }}">
+                            <a href="{{ route('fleet.fuel_usages.index') }}" class="nav-link">
                                 <i class="nav-icon fas fa-gas-pump"></i>
                                 <p>Fuel Usages</p>
                             </a>
                         </li>
-
+                        @endif
+                        @if ($usr->can('reports.view'))
                         <li class="nav-item">
                             <a href="{{ route('fleet.reports') }}" class="nav-link">
                                 <i class="nav-icon fas fa-chart-line"></i>
                                 <p>Reports</p>
                             </a>
                         </li>
+                        @endif
                     </ul>
                 </li>
+                @endif
 
                 <!-- Asset Management Module -->
                 <li class="nav-item">
@@ -148,55 +148,50 @@
                         </li>
                     </ul>
                 </li>
+                
+
                 <li class="nav-item">
                     <a href="{{ route('tasks.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-tasks"></i>
                         <p>Task Management</p>
                     </a>
                 </li>
+
                 
-                <!-- User and Role Permission Management Module -->
-                <li class="nav-item">
+                @if ($usr->can('user.view') || $usr->can('role.view') || $usr->can('permission.view'))
+                <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fa fa-users"></i>
-                        <p>
-                            User Management
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
+                        <p>User Management<i class="right fas fa-angle-left"></i></p>
                     </a>
                     <ul class="nav nav-treeview">
+                        @if ($usr->can('users.view'))
                         <li class="nav-item">
                             <a href="{{ route('users.index') }}" class="nav-link">
                                 <i class="nav-icon fas fa-users"></i>
                                 <p>Users</p>
                             </a>
                         </li>
-
+                        @endif
+                        @if ($usr->can('roles.view'))
                         <li class="nav-item">
                             <a href="{{ route('roles.index') }}" class="nav-link">
                                 <i class="nav-icon fas fa-user-tag"></i>
                                 <p>Roles</p>
                             </a>
                         </li>
-
+                        @endif
+                        @if ($usr->hasRole('admin'))
                         <li class="nav-item">
                             <a href="{{ route('permissions.index') }}" class="nav-link">
                                 <i class="nav-icon fas fa-key"></i>
                                 <p>Permissions</p>
                             </a>
                         </li>
+                        @endif
                     </ul>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    <i class="fa fa-home nav-icon"></i>
-                    <p>Logout</p>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </a>
-                </li>
-                
+                @endif
             </ul>
         </nav>
     </div>
